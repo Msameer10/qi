7d1eed2c14309a4e783a5feb74d8010c8e342f42
Sameer <mohammadsameer0409@gmail.com>
2023-10-06 14:59:16 -0500

    tbbt

--- template.js
+++ template.js
@@ -1,35 +1,166 @@
 {
-    name: "", //Quiz 2
-    questions: [
-        {
-            question: "Using which platform, can you board the Hogwarts Express?",
-            options: ["9 2/4", "9 3/4", "9 1/4"],
-            correctAnswer: 1
-        },
-        {
-            question: "Where was the 2014 FIFA World Cup Final held?",
-            options: ["Rio", "Brasilia", "Sao Paulo", "Salvador"],
-            correctAnswer: 0
-        },
-        {
-            question: "What was the medieval name of the city now known as Istanbul?",
-            options: ["Petra", "Ankara", "Constantinople", "Antioch"],
-            correctAnswer: 2
-        },
-        {
-            question: "What was the name of the project led by Robert J. Oppenheimer during World War II",
-            options: ["Apollo", "Brooklyn", "Manhattan", "Overlord"],
-            correctAnswer: 2
-        },
-        {
-            question: "As of 2023, which is the highest-rated movie on IMDb?",
-            options: [" The Shawshank Redemption", "The God Father", "Forest Gump", "The Dark Knight", "The Lord of the Rings: The Return of the King"],
-            correctAnswer: 0
-        },
-        {
-            question: "Which of the following terms doesn't even remotely signify 'King'?",
-            options: ["Rex", "Emir", "Khan", "Pharoh", "Sultan", "Tsar"],
-            correctAnswer: 1
-        }
-    ]
+name: "", //Quiz 2
+questions: [
+{
+    question: "Who is known for his catchphrase 'Bazinga!'?",
+    options: ["Sheldon Cooper", "Howard Wolowitz", "Rajesh Koothrappali", "Leonard Hofstadter"],
+    correctAnswer: 0
+},
+{
+    question: "What is the name of the actress who plays Penny?",
+    options: ["Kaley Cuoco", "Mayim Bialik", "Melissa Rauch", "Sara Gilbert"],
+    correctAnswer: 0
+},
+{
+    question: "Which character has a Ph.D. in astrophysics?",
+    options: ["Penny", "Rajesh Koothrappali", "Sheldon Cooper", "Howard Wolowitz"],
+    correctAnswer: 1
+},
+{
+    question: "What is the name of Howard's wife?",
+    options: ["Amy", "Emily", "Bernadette", "Leslie"],
+    correctAnswer: 2
+},
+{
+    question: "What is Sheldon's favorite spot to sit on the couch?",
+    options: ["The middle cushion", "The left end", "The right end", "Anywhere"],
+    correctAnswer: 1
+},
+{
+    question: "Which comic book character is a favorite of Sheldon?",
+    options: ["Superman", "Batman", "Spider-Man", "Wonder Woman"],
+    correctAnswer: 1
+},
+{
+    question: "What is the name of the university where the main characters work?",
+    options: ["Caltech", "MIT", "Stanford", "Harvard"],
+    correctAnswer: 0
+},
+{
+    question: "What is Raj's inability to speak to women unless he's drunk called?",
+    options: ["Shy Syndrome", "Mute Mode", "Silent Disorder", "Selective Mutism"],
+    correctAnswer: 3
+},
+{
+    question: "What is the name of Howard's aerospace company?",
+    options: ["AerospaceTech", "Rocket Science Inc.", "Wolowitz Industries", "Caltech Aerospace"],
+    correctAnswer: 2
+},
+{
+    question: "What is the title of the theme song for 'The Big Bang Theory'?",
+    options: ["'Science Is Fun'", "'Geeky Groove'", "'Nerds Rule'", "'The History of Everything'"],
+    correctAnswer: 3
+},
+{
+    question: "What is Penny's last name?",
+    options: ["Smith", "Johnson", "Hofstadter", "Unknown"],
+    correctAnswer: 3
+},
+{
+    question: "What is Sheldon's favorite video game?",
+    options: ["Call of Duty", "World of Warcraft", "Halo", "Minecraft"],
+    correctAnswer: 1
+},
+{
+    question: "What is the name of Raj's dog?",
+    options: ["Buddy", "Cinnamon", "Rover", "Comet"],
+    correctAnswer: 1
+},
+{
+    question: "Which character is an engineer who designs spacecraft?",
+    options: ["Sheldon Cooper", "Leonard Hofstadter", "Howard Wolowitz", "Rajesh Koothrappali"],
+    correctAnswer: 2
+},
+{
+    question: "What is the name of Sheldon's roommate before Leonard?",
+    options: ["Barry Kripke", "Stuart Bloom", "Howard Wolowitz", "Dennis Kim"],
+    correctAnswer: 3
+},
+{
+    question: "What is the name of Amy's boyfriend from the beginning of the series?",
+    options: ["Stuart", "Wil Wheaton", "Zack", "Dave"],
+    correctAnswer: 2
+},
+{
+    question: "What is the name of Penny's ex-boyfriend who was a musician?",
+    options: ["David", "Kurt", "Zack", "Dave"],
+    correctAnswer: 3
+},
+{
+    question: "Which character has a twin sister named Missy?",
+    options: ["Sheldon Cooper", "Leonard Hofstadter", "Howard Wolowitz", "Rajesh Koothrappali"],
+    correctAnswer: 0
+},
+{
+    question: "What does Sheldon always knock three times before doing?",
+    options: ["Entering rooms", "Sitting in his spot", "Speaking to Penny", "Playing video games"],
+    correctAnswer: 0
+},
+{
+    question: "What does Penny do for a living at the beginning of the series?",
+    options: ["Waitress", "Actress", "Scientist", "Teacher"],
+    correctAnswer: 0
+},
+{
+    question: "Which character is known for his love of doing magic?",
+    options: ["Sheldon Cooper", "Leonard Hofstadter", "Howard Wolowitz", "Rajesh Koothrappali"],
+    correctAnswer: 2
+},
+{
+    question: "What is the name of the comic book store where the characters often hang out?",
+    options: ["Nerd Haven", "Geek World", "Stuart's Comics", "Comic Galaxy"],
+    correctAnswer: 2
+},
+{
+    question: "Which character briefly worked as a waitress at The Cheesecake Factory?",
+    options: ["Penny", "Bernadette", "Amy", "Emily"],
+    correctAnswer: 1
+},
+{
+    question: "What is the name of Howard and Bernadette's son?",
+    options: ["Michael", "David", "Halley", "George"],
+    correctAnswer: 2
+},
+{
+    question: "What is the name of Sheldon's favorite online multiplayer game?",
+    options: ["World of Warcraft", "Halo", "Call of Duty", "Counter-Strike"],
+    correctAnswer: 0
+},
+{
+    question: "What is the title of the children's science show that Sheldon and Amy host?",
+    options: ["Science Explorers", "Fun with Flags", "Physics Phun", "Geeky Adventures"],
+    correctAnswer: 1
+},
+{
+    question: "What is the name of Leonard's high school bully who appears in a few episodes?",
+    options: ["Will Wheaton", "Barry Kripke", "Leslie Winkle", "Jimmy Speckerman"],
+    correctAnswer: 3
+},
+{
+    question: "Which character is known for his distinctive wardrobe, including his collection of colorful belt buckles?",
+    options: ["Sheldon Cooper", "Leonard Hofstadter", "Howard Wolowitz", "Rajesh Koothrappali"],
+    correctAnswer: 2
+},
+{
+    question: "What is the name of Penny's pharmaceutical sales job?",
+    options: ["Pfizer Sales Rep", "Zynerma Pharmaceuticals", "Caltech Sales Associate", "Bernadette's Pharmaceuticals"],
+    correctAnswer: 1
+},
+{
+    question: "What is the name of Howard's space mission that goes to the International Space Station?",
+    options: ["Apollo 13", "Discovery 7", "Expedition 42", "Voyager 1"],
+    correctAnswer: 2
+},
+{
+    question: "Which character has a mother who is often heard but never seen on the show?",
+    options: ["Sheldon Cooper", "Leonard Hofstadter", "Howard Wolowitz", "Rajesh Koothrappali"],
+    correctAnswer: 2
+},
+{
+    question: "Which character is an accomplished musician and plays various musical instruments throughout the series?",
+    options: ["Penny", "Rajesh Koothrappali", "Sheldon Cooper", "Howard Wolowitz"],
+    correctAnswer: 1
+},
+
+]
 },
